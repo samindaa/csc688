71c84da311231ffd4c3d99f70cb7383dc7f778af
samindaa <samindaa@gmail.com>
2014-02-18 20:03:12 -0500

    Tivia

--- csc688.ino
+++ csc688.ino
@@ -1,11 +1,16 @@
+#include "Framework.h"
+
 void setup()
 {
   // put your setup code here, to run once:
-  
+  Graph& graph = Graph::getInstance();
+  graph.computeGraph();
+  graph.topoSort();
+  graph.graphOutputInit();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  
+  graph.graphOutputUpdate();
 }
