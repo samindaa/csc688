5699f403a0d1464f61812f6675c2d86b726aee41
samindaa <samindaa@gmail.com>
2014-04-27 21:59:40 -0400

    formatted

--- Framework.h
+++ Framework.h
@@ -31,94 +31,94 @@
 template<typename T>
 class Vector
 {
-private:
-  int theSize;
-  int theCapacity;
-  T* objects;
+  private:
+    int theSize;
+    int theCapacity;
+    T* objects;
 
-public:
-  explicit Vector() :
-      theSize(0), theCapacity(0), objects(0)
-  {
-  }
+  public:
+    explicit Vector() :
+        theSize(0), theCapacity(0), objects(0)
+    {
+    }
 
-  Vector(const Vector& that) :
-      objects(0)
-  {
-    operator=(that);
-  }
+    Vector(const Vector& that) :
+        objects(0)
+    {
+      operator=(that);
+    }
 
-  ~Vector()
-  {
-    if (objects)
-      free(objects);
-  }
-
-  const Vector & operator=(const Vector& that)
-  {
-    if (this != &that)
+    ~Vector()
     {
       if (objects)
         free(objects);
-      objects = 0;
-      theSize = that.size();
-      theCapacity = that.theCapacity;
-
-      objects = (T*)malloc(capacity() * sizeof(T));
-      for (int k = 0; k < size(); k++)
-        objects[k] = that.objects[k];
-
     }
 
-    return *this;
-  }
+    const Vector & operator=(const Vector& that)
+    {
+      if (this != &that)
+      {
+        if (objects)
+          free(objects);
+        objects = 0;
+        theSize = that.size();
+        theCapacity = that.theCapacity;
 
-  void resize(int newSize)
-  {
-    if (newSize > theCapacity)
-      reserve(newSize * 2 + 1);
-    theSize = newSize;
-  }
+        objects = (T*)malloc(capacity() * sizeof(T));
+        for (int k = 0; k < size(); k++)
+          objects[k] = that.objects[k];
 
-  void reserve(int newCapacity)
-  {
-    if (newCapacity < theSize)
-      return;
+      }
 
-    T *oldArray = objects;
+      return *this;
+    }
 
-    objects =  (T*)malloc(newCapacity * sizeof(T));
-    for (int k = 0; k < theSize; k++)
-      objects[k] = oldArray[k];
+    void resize(int newSize)
+    {
+      if (newSize > theCapacity)
+        reserve(newSize * 2 + 1);
+      theSize = newSize;
+    }
 
-    theCapacity = newCapacity;
+    void reserve(int newCapacity)
+    {
+      if (newCapacity < theSize)
+        return;
 
-    if (oldArray)
-     free(oldArray);
-  }
+      T *oldArray = objects;
 
-  T& operator[](int index) { return objects[index]; }
-  const T& operator[](int index) const { return objects[index]; }
-  bool empty() const { return size() == 0;  }
-  int size() const { return theSize; }
-  int capacity() const { return theCapacity; }
+      objects =  (T*)malloc(newCapacity * sizeof(T));
+      for (int k = 0; k < theSize; k++)
+        objects[k] = oldArray[k];
 
-  void push_back(T x)
-  {
-    if (theSize == theCapacity)
-      reserve(2 * theCapacity + 1);
-    objects[theSize++] = x;
-  }
+      theCapacity = newCapacity;
 
-  const T& front() const { return objects[0]; }
-  T& front() { return objects[0]; }
+      if (oldArray)
+       free(oldArray);
+    }
 
-  void erase(int index)
-  {
-    for (int i = index; i < size() - 1; ++i)    // for each item that follows 'index'
-      objects[i] = objects[i + 1];            // shift the item down one slot in memory
-    --theSize;
-  }
+    T& operator[](int index) { return objects[index]; }
+    const T& operator[](int index) const { return objects[index]; }
+    bool empty() const { return size() == 0;  }
+    int size() const { return theSize; }
+    int capacity() const { return theCapacity; }
+
+    void push_back(T x)
+    {
+      if (theSize == theCapacity)
+        reserve(2 * theCapacity + 1);
+      objects[theSize++] = x;
+    }
+
+    const T& front() const { return objects[0]; }
+    T& front() { return objects[0]; }
+
+    void erase(int index)
+    {
+      for (int i = index; i < size() - 1; ++i)    // for each item that follows 'index'
+        objects[i] = objects[i + 1];            // shift the item down one slot in memory
+      --theSize;
+    }
 
     void purge()
     {
